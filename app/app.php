ac88c742e01eeac238aa5292699ae24ac84206ab
Jenna Maloney <jmalo@uw.edu>
2015-10-06 15:30:16 -0700

    add save method to Task class

--- app/app.php
+++ app/app.php
@@ -12,14 +12,9 @@
 
     $app->get("/", function()
     {
-        $test_task = new Task("Learn PHP.");
-        $another_test_task = new Task("Learn Drupal.");
-        $third_task = new Task("Visit France.");
-
-        $list_of_tasks = array($test_task, $another_test_task, $third_task);
         $output = "";
 
-        foreach ($list_of_tasks as $task)
+        foreach (Task::getAll() as $task)
         {
             $output = $output . "<p>" . $task->getDescription() . "</p>";
         }
