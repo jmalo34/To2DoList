0a2cfa87421d27c1bcdb8032ab18f586ef89f7ed
Jenna Maloney <jmalo@uw.edu>
2015-10-06 15:40:23 -0700

    display input form

--- app/app.php
+++ app/app.php
@@ -14,13 +14,36 @@
     {
         $output = "";
 
+        //loop through Tasks stored in session and print descriptions
         foreach (Task::getAll() as $task)
         {
             $output = $output . "<p>" . $task->getDescription() . "</p>";
         }
 
+        //display form that when submitted, will create a new instance of the Task class. Notice the form method is set to "post".
+        $output = $output . "
+            <form action='/tasks' method='post'>
+                <label for='description'>Task Description</label>
+                <input id='description' name='description' type='text'>
+
+                <button type='submit'>Add task</button>
+            </form>
+            ";
+
         return $output;
     });
+
+    $app->post("/tasks", function()
+    {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return
+            "
+            <h1>You created a task!</h1>
+            <p>" . $task->getDescription() . "</p>
+            <p><a href='/'>View your list of things to do.</a></p>
+            ";
+    })
 
     return $app;
  ?>
