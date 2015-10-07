786535fb805cd3441e82510a7ea88af1bf1cdef2
Jenna Maloney <jmalo@uw.edu>
2015-10-06 17:51:10 -0700

    create template for create task route

--- app/app.php
+++ app/app.php
@@ -20,20 +20,15 @@
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    //route for URL at '/tasks'
-    $app->post("/tasks", function()
+    //route for URL at '/tasks'; pass $app variable into route with the keyword 'use'
+    $app->post("/tasks", function() use ($app)
     {
         //since form action was set to POST method, data will be read out of the form by using the superglobal $_POST variable. Here, we instatiate an instance of Task.
         $task = new Task($_POST['description']);
         //pass(?) THIS instance of Task into the save method from Task class
         $task->save();
         //return text containing the task description, + also, a link to go see a list of all the tasks (located at home, AKA '/')
-        return
-            "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-            ";
+        return $app['twig']->render('create_task.html.twig');
     });
 
     $app->post("delete_tasks", function()
